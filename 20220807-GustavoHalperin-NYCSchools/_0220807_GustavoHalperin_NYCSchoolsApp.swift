96b09b9808e8743bf04b6116ce971f749dd2bd86
ideastouch <gustavoh@ideasouch.com>
2022-08-07 21:56:21 -0700

    Initial Commit

---
+++ 20220807-GustavoHalperin-NYCSchools/_0220807_GustavoHalperin_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0220807_GustavoHalperin_NYCSchoolsApp.swift
+//  20220807-GustavoHalperin-NYCSchools
+//
+//  Created by Gustavo Halperin on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct _0220807_GustavoHalperin_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
